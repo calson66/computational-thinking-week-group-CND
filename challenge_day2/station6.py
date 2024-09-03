b753ec5689d129416de2800825f675b6cdec9e35
陈某 <roychen@re-byods-146-50-154-82.wireless.uva.nl>
2024-09-03 10:47:15 +0200

    This is a message describing what I did

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -1,9 +1,6 @@
 import numpy as np
 
-
 def solution_station_6(input):
-    a = 0.9994
-    b = 1.0004
-    c = -0.0006
-    d = 0.0006
-    return a * np.sin(b * input + c) + d
+
+    output = round(1.0000 * np.sin(0.99999 * input) - 0.00004,4)
+    return output
